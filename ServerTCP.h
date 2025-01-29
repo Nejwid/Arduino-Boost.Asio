a81d253761867e50fced11535720f52ebfbe5cd9
D N <127353677+Nejwid@users.noreply.github.com>
2025-01-30 00:18:52 +0100

    Create ServerTCP.h

---
+++ ServerTCP.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <boost/asio.hpp>
+#include <string>
+#include <iostream>
+#include "json.hpp"
+#include <vector>
+#include <memory>
+
+using namespace std;
+using net = boost::asio::ip::tcp;
+using json = nlohmann::json;
+
+namespace TCP{
+    class Server{ // instancja działająca na teoretycznie nieskończonej pętli 
+    private:
+        json verificationKey = "fjuaydgia3y79e698";
+        vector<string> messages;
+        net::acceptor acceptor;
+        boost::asio::io_service io_service;
+
+        bool verification(const string& key);
+        void manageConnections();
+        void handleClient(net::socket socket);
+
+    public:
+        Server(short port);
+        void runServer();
+        void getMessages() const;
+        ~Server();
+    };
+}
