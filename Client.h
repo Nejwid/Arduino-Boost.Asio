e8be56ea17e83358492b6d61ed8bcebf95b792d8
D N <127353677+Nejwid@users.noreply.github.com>
2025-01-30 00:15:03 +0100

    Create Client.h

---
+++ Client.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <boost/asio.hpp>
+#include <memory>
+#include <iostream>
+#include <vector>
+#include <string>
+#include "ArduinoConnector.h"
+
+using namespace std;
+using net = boost::asio::ip::tcp;
+
+namespace client {
+    class Client {
+    private:
+        boost::asio::io_service io_service;
+        net::socket socket;
+        string ip;
+        short port;
+        vector<string> messages;
+
+    public:
+        Client(const string& s, const short& t, const string& key);
+        void sendMessage();
+        void setMessages(vector<string> temp);
+    };
+}
